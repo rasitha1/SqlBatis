2b98a2805d9cc19cddbcd255a92bc2eff3a71759
Rasitha Wijesinghe <rasithaw@okloworld.com>
2020-12-27 19:38:15 -0600

    trying sql 2017

--- setupdb.cmd
+++ setupdb.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Setting up SqlLocalDB database instance...
-sqllocaldb create "SqlBatis" 15.0 -s
+sqllocaldb create "SqlBatis" 14.0 -s
 echo Executing MSSQL Database Init Script...
 sqlcmd -S "(localdb)\SqlBatis" -E -i .\test\SqlBatis.DataMapper.Test\Scripts\MSSQL\DataBase.sql
 
