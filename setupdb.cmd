ced3a4763af8595305ee3a71940d8e433b07b584
Rasitha Wijesinghe <rasithaw@okloworld.com>
2020-12-27 21:01:07 -0600

    no need for sql version

--- setupdb.cmd
+++ setupdb.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Setting up SqlLocalDB database instance...
-sqllocaldb create "SqlBatis" 13.0 -s
+sqllocaldb create "SqlBatis" -s
 echo Executing MSSQL Database Init Script...
 sqlcmd -S "(localdb)\SqlBatis" -E -i .\test\SqlBatis.DataMapper.Test\Scripts\MSSQL\DataBase.sql
 
