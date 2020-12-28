8690bb0495493d71b0fd714eb231aa28435c98bc
Rasitha Wijesinghe <rasithaw@okloworld.com>
2020-12-27 19:33:14 -0600

    fixing issue with cmd file

--- setupdb.cmd
+++ setupdb.cmd
@@ -1,11 +1,7 @@
 @echo off
 echo Setting up SqlLocalDB database instance...
-set db=sqllocaldb info SqlBatis
-if "%db%"=="" sqllocaldb create "SqlBatis" 15.0 -s
-set db=sqllocaldb info SqlBatis | findstr "Running"
-if "%db%"=="" sqllocaldb s "SqlBatis"
-
+sqllocaldb create "SqlBatis" 15.0 -s
 echo Executing MSSQL Database Init Script...
-sqlcmd -S (localdb)\SqlBatis -E -i .\test\SqlBatis.DataMapper.Test\Scripts\MSSQL\DataBase.sql
+sqlcmd -S "(localdb)\SqlBatis" -E -i .\test\SqlBatis.DataMapper.Test\Scripts\MSSQL\DataBase.sql
 
 echo Done
