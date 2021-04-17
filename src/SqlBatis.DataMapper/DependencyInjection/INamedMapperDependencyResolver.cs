e62e999f754be8d5800ca1ade0440fafa098e25b
rasitha <rasitha.wijesinghe@gmail.com>
2021-04-17 09:21:32 -0500

    Making it easier to get a type that depends on a named ISqlMapper

---
+++ src/SqlBatis.DataMapper/DependencyInjection/INamedMapperDependencyResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SqlBatis.DataMapper.DependencyInjection
+{
+    /// <summary>
+    /// A dependency resolver that can create a type from the default <see cref="IServiceProvider"/>
+    /// using named <see cref="ISqlMapper"/> registrations
+    /// </summary>
+    public interface INamedMapperDependencyResolver
+    {
+        /// <summary>
+        /// Gets an instance of <typeparamref name="TImplementation"/> using a named <see cref="ISqlMapper"/>
+        /// instance (registered in DI via <code>services.AddSqlMapper(name)</code>) 
+        /// </summary>
+        /// <typeparam name="TImplementation"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        TImplementation GetInstance<TImplementation>(string name);
+    }
+}
---
+++ src/SqlBatis.DataMapper/DependencyInjection/NamedMapperDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SqlBatis.DataMapper.DependencyInjection
+{
+    /// <summary>
+    /// Default <see cref="INamedMapperDependencyResolver"/>
+    /// </summary>
+    internal class NamedMapperDependencyResolver : INamedMapperDependencyResolver
+    {
+        private readonly IServiceProvider _provider;
+        private readonly ISqlMapperFactory _mapperFactory;
+
+        private readonly ConcurrentDictionary<Type, ObjectFactory> _factoryMap =
+            new ConcurrentDictionary<Type, ObjectFactory>();
+
+        public NamedMapperDependencyResolver(IServiceProvider provider, ISqlMapperFactory mapperFactory)
+        {
+            _provider = provider;
+            _mapperFactory = mapperFactory;
+        }
+        public TImplementation GetInstance<TImplementation>(string name)
+        {
+            var mapper = _mapperFactory.GetMapper(name);
+            if (!_factoryMap.TryGetValue(typeof(TImplementation), out var objectFactory))
+            {
+                objectFactory = ActivatorUtilities.CreateFactory(typeof(TImplementation), new[] { typeof(ISqlMapper) });
+                _factoryMap.TryAdd(typeof(TImplementation), objectFactory);
+            }
+
+            return (TImplementation)objectFactory(_provider, new object[] { mapper });
+        }
+    }
+}
