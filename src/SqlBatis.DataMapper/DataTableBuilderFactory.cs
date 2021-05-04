3f4ad909770077d297c02432cc939db0bcc2a609
rasitha <rasitha.wijesinghe@gmail.com>
2021-05-03 23:25:55 -0500

    Adding support for SQL Server Structured parameters

---
+++ src/SqlBatis.DataMapper/DataTableBuilderFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace SqlBatis.DataMapper
+{
+    internal class DataTableBuilderFactory : IDataTableBuilderFactory
+    {
+        public IDataTableBuilder<T> CreateBuilder<T>()
+        {
+            return new DataTableBuilder<T>();
+        }
+    }
+}
---
+++ src/SqlBatis.DataMapper/IDataTableBuilderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace SqlBatis.DataMapper
+{
+    /// <summary>
+    /// A factory that can return a <see cref="IDataTableBuilder{T}"/>
+    /// </summary>
+    public interface IDataTableBuilderFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="IDataTableBuilder{T}"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        IDataTableBuilder<T> CreateBuilder<T>();
+    }
+}
